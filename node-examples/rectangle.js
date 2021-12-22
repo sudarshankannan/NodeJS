769f8c7b2ba37d370bdf62f19cff70d6c5f0d696
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-12-21 22:02:37 -0800

    hello

---
+++ node-examples/rectangle.js
@@ -0,0 +1,15 @@
+//callback = module supplied when func is called
+module.exports = (x,y,callback) => {
+    if(x <= 0 || y <= 0){
+        setTimeout(() =>
+            callback(new Error("Rectangle dimensions should be > 0")),
+            2000);
+    } else {
+        setTimeout(() =>
+            callback(null, {
+                perimeter = (x,y) => (2*(x+y)),
+                area = (x,y) => (x*y)
+            }), 
+            2000);
+    }
+}

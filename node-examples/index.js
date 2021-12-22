f21f6aa125163c527240451627a3f00c4051c912
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-12-21 21:22:46 -0800

    fixed syntax error

--- node-examples/index.js
+++ node-examples/index.js
@@ -4,7 +4,7 @@ var rect = {
 };
 
 function solveRect(l,b){
-    console.log("Solving for rectangle with l = " + l + "and b = " b);
+    console.log("Solving for rectangle with l = " + l + "and b = " + b);
     if (l<=0 || b<=0){
         console.log("Rectangle dimensions should be greater than zero:")
     }
